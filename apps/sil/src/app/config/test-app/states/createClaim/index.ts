7edc8904809f822780e94b56fe3cd1e9af40c0ab
Enoch Abassey <enochabassey@gmail.com>
2020-09-28 17:23:07 +0000

    structure nodes

--- apps/sil/src/app/config/test-app/states/createClaim/index.ts
+++ apps/sil/src/app/config/test-app/states/createClaim/index.ts
@@ -1,12 +1,7 @@
 import { StateConfig } from '@wilo';
-import {policyLookup} from './nodes/policyLookup';
-import {policyDetails} from './nodes/policyDetails';
-import {maps} from './nodes/maps';
-import {claimDetails} from './nodes/claimDetails';
-import {appointments} from './nodes/appointments';
-import {excess} from './nodes/excess';
-import {history} from './nodes/history';
-import {submit} from './nodes/submit';
+import { navigateToWorkflowButton } from '../../shared';
+import * as TP from '@indigo/templates';
+
 
 export const createClaim: StateConfig = {
     id: 'createClaim',
@@ -15,13 +10,162 @@ export const createClaim: StateConfig = {
     startNode: 'policyLookup',
     showTabs: true,
     nodes: {
-      policyLookup,
-      policyDetails,
-      maps,
-      claimDetails,
-      appointments,
-      excess,
-      history,
-      submit
+      policyLookup: {
+        name: 'Policy Lookup',
+        component: {
+          children:[
+            {
+              component: TP.Node2Component,
+              inputs: {
+                text: 'Dummy screen for Policy Lookup'
+              }
+            },
+          ]
+        },
+        navs: [
+          navigateToWorkflowButton,
+          {text: 'Continue', routerLink: ['policyDetails'], location: 'right'}
+        ],
+        footerType: 'node_nav'
+      },
+      policyDetails: {
+        name: 'Policy Details',
+        component: {
+          children:[
+            {
+              component: TP.Node2Component,
+              inputs: {
+                text: 'Dummy screen for Policy Details'
+              }
+            },
+          ]
+        },
+        navs: [
+          navigateToWorkflowButton,
+          {text: 'Back', routerLink: ['policyLookup'], location: 'right'},
+          {text: 'Continue', routerLink: ['maps'], location: 'right'}
+        ],
+        footerType: 'node_nav'
+      },
+      maps: {
+        name: 'Maps',
+        component: {
+          children:[
+            {
+              component: TP.Node2Component,
+              inputs: {
+                text: 'Dummy screen for Maps'
+              }
+            },
+          ]
+        },
+        navs: [
+          navigateToWorkflowButton,
+          {text: 'Back', routerLink: ['policyDetails'], location: 'right'},
+          {text: 'Continue', routerLink: ['claimDetails'], location: 'right'}
+        ],
+        footerType: 'node_nav'
+      },
+      claimDetails: {
+        name: 'Claim Details',
+        component: {
+          children:[
+            {
+              component: TP.Node2Component,
+              inputs: {
+                text: 'Dummy screen for Claim Details'
+              }
+            },
+          ]
+        },
+        navs: [
+          navigateToWorkflowButton,
+          {text: 'Back', routerLink: ['maps'], location: 'right'},
+          {text: 'Continue', routerLink: ['appointments'], location: 'right'}
+
+        ],
+        footerType: 'node_nav'
+      },
+      appointments: {
+        name: 'Appointments',
+        component: {
+          children:[
+            {
+              component: TP.Node2Component,
+              inputs: {
+                text: 'Dummy screen for Appointments'
+              }
+            },
+          ]
+        },
+        navs: [
+          navigateToWorkflowButton,
+          {text: 'Back', routerLink: ['claimDetails'], location: 'right'},
+          {text: 'Continue', routerLink: ['excess'], location: 'right'}
+
+        ],
+        footerType: 'node_nav'
+      },
+      excess: {
+        name: 'Excess',
+        component: {
+          children:[
+            {
+              component: TP.Node2Component,
+              inputs: {
+                text: 'Dummy screen for Excess'
+              }
+            },
+          ]
+        },
+        navs: [
+          navigateToWorkflowButton,
+          {text: 'Back', routerLink: ['appointments'], location: 'right'},
+          {text: 'Continue', routerLink: ['history'], location: 'right'}
+
+        ],
+        footerType: 'node_nav'
+      },
+      history: {
+        name: 'History',
+        component: {
+          children:[
+            {
+              component: TP.Node2Component,
+              inputs: {
+                text: 'Dummy screen for History'
+              }
+            },
+          ]
+        },
+        navs: [
+          navigateToWorkflowButton,
+          {text: 'Back', routerLink: ['excess'], location: 'right'},
+          {text: 'Continue', routerLink: ['submit'], location: 'right'}
+
+        ],
+        footerType: 'node_nav'
+      },
+      submit: {
+        name: 'Submit',
+        component: {
+          children:[
+            {
+              component: TP.Node2Component,
+              inputs: {
+                text: 'Dummy screen for Submit'
+              }
+            },
+          ]
+        },
+        navs: [
+          navigateToWorkflowButton,
+          {text: 'Back', routerLink: ['history'], location: 'right'},
+          {text: 'Save As Draft', location: 'right'},
+          {text: 'Submit', location: 'right'},
+
+        ],
+        footerType: 'node_nav'
+      }
     }
 }
