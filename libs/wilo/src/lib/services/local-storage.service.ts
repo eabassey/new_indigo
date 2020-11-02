72707ebd846e636c407af154a4166c06f32fe972
Enoch Abassey <enochabassey@gmail.com>
2020-11-02 23:00:38 +0000

    adding local storage service

--- libs/wilo/src/lib/services/local-storage.service.ts
+++ libs/wilo/src/lib/services/local-storage.service.ts
@@ -4,8 +4,13 @@ import { Injectable } from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class LocalStorageService {
 
+  setItem = window.localStorage.setItem;
+  removeItem = window.localStorage.removeItem;
+  key = window.localStorage.key;
+  clear = window.localStorage.clear;
   getItem(key: string) {
     const itemStr = window.localStorage.getItem(key);
     return itemStr.startsWith("{") ? JSON.parse(itemStr) : itemStr;
   }
+
 }
